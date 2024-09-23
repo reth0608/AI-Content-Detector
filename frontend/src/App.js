052e85e5c105e0108f70b19b918f8da6cc13ce59
KshithijKeshav <kshithijkeshav@gmail.com>
2024-09-24 02:22:13 +0530

    Initial frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
+// App.js
+import React, { useState } from "react";
+import "./App.css"; // Importing CSS
 
-import './App.css';
+function InputBox(){
+  return(
+    <div className="input-box">
+      <form>
+        <textarea type="text" placeholder="Here goes nothing" ></textarea>
+        <br/>
+        <button class="save" type="submit">Check</button>
+      </form>
+    </div>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
+  return(<>
+    
+    <div className="Central-text">
+    <InputBox></InputBox>
+    <t>place holder</t>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
