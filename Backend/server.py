7335fca35e4d07e5d4eb5fbcf297a48a4d1aaf6b
KshithijKeshav <145021571+KshithijKeshav@users.noreply.github.com>
2024-09-25 01:19:48 +0530

    Removed everything

--- Backend/server.py
+++ Backend/server.py
@@ -1,16 +1,1 @@
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from flask import Flask,request,jsonify
-from flask_cors import CORS
 
-tokenizer = AutoTokenizer.from_pretrained("idajikuu/AI-HUMAN-detector")
-model = AutoModelForSequenceClassification.from_pretrained("idajikuu/AI-HUMAN-detector")
-user_input=list(input('Enter text\n').split('\n'))
-inputs=tokenizer(user_input,return_tensors="pt")
-with torch.no_grad():
-    logits=model(**inputs).logits
-predicted_realness = logits.argmax().item()
-if(model.config.id2label[predicted_realness]=='Fake'):
-    print("humannn")
-else:
-    print("It is AI")
